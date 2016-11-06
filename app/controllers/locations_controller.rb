1d86e3f9296b6ddcc95e1eaed569efa5c43cb3d0
Murugan <murugu2012@gmail.com>
2016-11-06 22:11:22 +0530

    Generate locations resource with starter:resource

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,63 @@
+class LocationsController < ApplicationController
+  def index
+    @locations = Location.all
+
+    render("locations/index.html.erb")
+  end
+
+  def show
+    @location = Location.find(params[:id])
+
+    render("locations/show.html.erb")
+  end
+
+  def new
+    @location = Location.new
+
+    render("locations/new.html.erb")
+  end
+
+  def create
+    @location = Location.new
+
+    @location.name = params[:name]
+    @location.address = params[:address]
+
+    save_status = @location.save
+
+    if save_status == true
+      redirect_to(:back, :notice => "Location created successfully.")
+    else
+      render("locations/new.html.erb")
+    end
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+
+    render("locations/edit.html.erb")
+  end
+
+  def update
+    @location = Location.find(params[:id])
+
+    @location.name = params[:name]
+    @location.address = params[:address]
+
+    save_status = @location.save
+
+    if save_status == true
+      redirect_to(:back, :notice => "Location updated successfully.")
+    else
+      render("locations/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+
+    @location.destroy
+
+    redirect_to(:back, :notice => "Location deleted.")
+  end
+end
