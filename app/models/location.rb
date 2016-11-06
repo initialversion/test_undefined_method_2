5125d6c91344e8f0b5bd070418acd61ee919c5c3
Murugan <murugu2012@gmail.com>
2016-11-06 22:11:40 +0530

    Direct association between locations and notes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,9 @@
 class Location < ApplicationRecord
   # Direct associations
 
+  has_many   :notes,
+             :dependent => :destroy
+
   has_many   :bookmarks,
              :dependent => :destroy
 
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,8 @@
 class Note < ApplicationRecord
   # Direct associations
 
+  belongs_to :location
+
   belongs_to :user
 
   # Indirect associations
