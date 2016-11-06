c07678678c88c46a5c2f07ed32f2bfbd117f054f
Murugan <murugu2012@gmail.com>
2016-11-06 22:11:40 +0530

    Direct association between users and requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :received_requests,
+             :class_name => "Request",
+             :foreign_key => "recipient_id",
+             :dependent => :destroy
+
   has_many   :sent_requests,
              :class_name => "Request",
              :foreign_key => "sender_id",
